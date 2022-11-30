d1310b311e9599a77491f1f53fe7de92a9b3aa3c
syafaat <syafaatmustofa46@gmail.com>
2022-11-30 11:35:48 +0700

    update

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="card rounded d-flex p-2" style="width: 18rem;">
+<div class="container px-4 text-center">
+    <div class="row gx-5">
         @foreach ($data as $item)
-        <img src="{{ asset('storage/'.$item->cover) }}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">{{ $item->judul }}</h5>
-            <p class="card-text">{{ $item->sinopsis }}</p>
-            <p class="card-text">{{ $item->penerbit }} , {{ $item->isbn }}</p>
-            <a href="{{ route('buku.show', $item->id) }}" class="btn btn-primary">Detail</a>
+        <div class="col">
+            <div class="card rounded d-flex p-2" style="width: 18rem;">
+                <img src="{{ asset('storage/'.$item->cover) }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $item->judul }}</h5>
+                    <p class="card-text">{{ $item->sinopsis }}</p>
+                    <p class="card-text">{{ $item->penerbit }} , {{ $item->isbn }}</p>
+                    <a href="{{ route('buku.show', $item->id) }}" class="btn btn-primary">Detail</a>
+                </div>
+            </div>
         </div>
         @endforeach
     </div>
